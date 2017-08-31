be7ec95c3144fb5b01657c4d21a7629fd4828612
sentamu <sentsjman1@gmail.com>
2017-08-31 11:43:21 +0300

    reserve

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :employees
   resources :reserves
   get 'welcome/homepage'
   get 'welcome/acc'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831081121) do
+ActiveRecord::Schema.define(version: 20170831083929) do
+
+  create_table "employees", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reserves", force: :cascade do |t|
     t.string "name"
